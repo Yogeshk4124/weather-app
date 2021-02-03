90e68c235ec97106a21d609c07889b6cea9bb9a5
Yogesh Kumar <yogeshk4124@gmail.com>
2021-02-03 15:07:12 +0530

    Update WeatherAPI.dart

--- lib/WeatherAPI.dart
+++ lib/WeatherAPI.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherAPI{
-  String apikey="74d03fa36147c48fcc230cd738144e66";
+  String apikey="apikey";
   Future callAPI(double lat,double lon) async{
     print("here:"+lat.toString()+" "+lon.toString());
     // http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apikey&units=metric');
