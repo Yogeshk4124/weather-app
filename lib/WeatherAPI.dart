b66efc87053b4c612e986dd45ea2116ecc8a9247
Yogesh Kumar <yogeshk4124@gmail.com>
2021-02-09 11:53:15 +0530

    Update WeatherAPI.dart

--- lib/WeatherAPI.dart
+++ lib/WeatherAPI.dart
@@ -4,8 +4,6 @@ import 'package:http/http.dart' as http;
 class WeatherAPI{
   String apikey="apikey";
   Future callAPI(double lat,double lon) async{
-    print("here:"+lat.toString()+" "+lon.toString());
-    // http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apikey&units=metric');
     http.Response response = await http.get('https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=alert,minutely,daily&appid=$apikey&units=metric');
     if(response.statusCode==200) {
       String data=(response.body);
@@ -14,4 +12,24 @@ class WeatherAPI{
     }
     else print(response.statusCode);
   }
+
+  Future callAreaAPI(double lat,double lon) async{
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apikey&units=metric');
+    if(response.statusCode==200) {
+      String data=(response.body);
+      var decodedData=jsonDecode(data);
+      return decodedData['name'];
+    }
+    else print(response.statusCode);
+  }
+  Future searchCallAPI(String place) async{
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?q=$place&appid=$apikey&units=metric');
+    if(response.statusCode==200) {
+      String data=(response.body);
+      var decodedData=jsonDecode(data);
+      return decodedData;
+    }
+    else print(response.statusCode);
+  }
+
 }
